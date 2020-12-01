6a2f475e29eb4687bc0935bcca26b9bfa7786bdf
Reda <reda.ieslaslagunas@gmail.com>
2020-12-01 16:40:32 +0100

    Mejorando ejercicio JDBC !

--- src/main/java/cat/copernic/m03uf06review/jdbc/JdbcMain.java
+++ src/main/java/cat/copernic/m03uf06review/jdbc/JdbcMain.java
@@ -5,6 +5,14 @@
  */
 package cat.copernic.m03uf06review.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * En aquesta secció cal accedir a una taula de MySQL amb un camp de cada tipus:
  * 
@@ -22,6 +30,28 @@ public class JdbcMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+            String usuario = "root";
+        String clave = "root";
+        String url = "jdbc:mysql://localhost:3306/institut";
+        Connection con;
+        Statement stmt;
+        ResultSet rs;
+
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
+                    + "institut?useUnicode=true&useJDBCCompliantTimezoneShift=true&"
+                    + "useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root");
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("Select * From alumnat");
+            rs.next();
+            do { // ID dni nom cognoms data_naixement
+                System.out.println(rs.getInt("id")+" "+rs.getString("dni")+" "+rs.getString("nom") + " " + rs.getString("cognoms")+" "+rs.getDate("data_naixement"));
+            } while (rs.next());
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JdbcMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
